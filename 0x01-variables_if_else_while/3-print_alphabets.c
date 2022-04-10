0dbab95bee2fe7f48b617d1e388825bceb80564c
Theophilus Ankamah <theoankamah66@gmail.com>
2022-04-10 03:19:04 -0700

    task 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,17 +9,18 @@ int main(void)
 {
 	char lower = 'a';
 	char upper = 'A';
+
 	while (lower <= 'z')
 	{
 		putchar(lower);
 		lower++;
 	}
+
 	while (upper <= 'Z') 
 	{
 		putchar(upper);
 		upper++;
 	}
 	putchar('\n');
-	
 	return (0);
 }
