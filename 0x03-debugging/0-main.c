54918ddf29a1f8625ae198933fc97e46a5906694
Theophilus Ankamah <theoankamah66@gmail.com>
2022-04-14 15:11:01 -0700

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** main - text functions that prints if integer is positive or negative
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positve_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+this readme is about debugging in c programming
