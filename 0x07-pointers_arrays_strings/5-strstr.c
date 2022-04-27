c365315179166a4e58718f92669824fd7bb60643
Theophilus Ankamah <theoankamah66@gmail.com>
2022-04-27 11:15:30 -0700

    task 6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,24 +2,34 @@
 #define NULL 0
 
 /**
- * _strpbrk - return pointer to byte in s that matches a byte in accept
- * @s: string to search
- * @accept: target matches
- * Return: pointer to index of string at first occurence
+ * _strstr - locate and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ * Return: pointer to index of string at first occurence of whole substring
  */
 
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j;
+	int i = 0, j, x;
 
-	while (s[i] != '\0')
+	if (needle[0] == '\0')
+		return (haystack);
+
+	while (haystack[i] != '\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		if (haystack[i] == needle[0])
 		{
-			if (s[i] == accept[j])
+			x = i, j = 0;
+			while (needle[j] != '\0')
 			{
-				s = &s[i];
-				return (s);
+				if (haystack[x] == needle[j])
+					x++, j++;
+				else
+					break;
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
 			}
 		}
 		i++;
