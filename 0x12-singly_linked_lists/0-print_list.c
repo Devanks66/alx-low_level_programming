63efc68b2415fa589e7ccbde30d0df09129eeb70
Theophilus Ankamah <theoankamah66@gmail.com>
2022-06-09 07:54:26 -0700

    task1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,6 +7,7 @@
  *
  * Return: number of nodes in the list
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
@@ -17,7 +18,6 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
 		count++;
 		h = h->next;
 	}
