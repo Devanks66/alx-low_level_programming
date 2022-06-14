6fb44b8edc78d2afb4664ad1801e7551b6850d38
Theophilus Ankamah <theoankamah66@gmail.com>
2022-06-14 03:25:54 -0700

    task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
-
-/**
- * dog_t - Typedef for struct dog
- */
+#ifndef DOG_H_
+#define DOG_H_
 
 /**
  * struct dog - structure for dogs
@@ -18,8 +14,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
